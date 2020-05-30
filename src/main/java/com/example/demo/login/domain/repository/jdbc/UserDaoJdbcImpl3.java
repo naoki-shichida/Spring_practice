a0ae7b06ef57e1c04aa39d2aa6bcc825aab9097b
naoki-shichida <azulexusia@yahoo.co.jp>
2020-05-30 13:24:43 +0900

    Spring入門4章～8章(DI実装、データバインド、バリデーション、AOP実装、CRUD操作）

---
+++ src/main/java/com/example/demo/login/domain/repository/jdbc/UserDaoJdbcImpl3.java
@@ -0,0 +1,44 @@
+package com.example.demo.login.domain.repository.jdbc;
+
+import com.example.demo.login.domain.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository("UserDaoJdbcImpl3")
+public class UserDaoJdbcImpl3 extends UserDaoJdbcImpl {
+
+    @Autowired
+    private JdbcTemplate jdbc;
+
+    //ユーザー1件取得
+    @Override
+    public User selectOne(String userId) {
+
+        //1件取得用SQL
+        String sql = "SELECT * FROM m_user WHERE user_id = ?";
+
+        //ポイント：BeanPropertyMapper,RowMapperの生成
+        RowMapper<User> rowMapper = new BeanPropertyRowMapper<User>(User.class);
+
+        //SQl実行
+        return jdbc.queryForObject(sql, rowMapper, userId);
+    }
+
+    //ユーザー全件取得
+    @Override
+    public List<User> selectMany() {
+
+        //M_USERテーブルのデータを全件取得するSQL
+        String sql = "SELECT * FROM m_user";
+
+        //RowMapperの生成
+        RowMapper<User> rowMapper = new BeanPropertyRowMapper<User>(User.class);
+
+        //SQL実行
+        return jdbc.query(sql, rowMapper);
+    }
+}
